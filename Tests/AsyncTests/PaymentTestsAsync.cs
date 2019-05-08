dd2a5d7cdeb92e94fdbcbc5ef89afa55d7b8074d
Stephen Barry <sbarry@trustev.com>
2019-05-08 13:00:56 +0100

    Update payment types to include new payment methods

--- Tests/AsyncTests/PaymentTestsAsync.cs
+++ Tests/AsyncTests/PaymentTestsAsync.cs
@@ -17,7 +17,7 @@ namespace Tests.AsyncTests
     public class PaymentTestsAsync : TestBase
     {
         [TestMethod]
-        public async Task PaymentTest_PostAsync_200()
+        public async Task PaymentTest_Card_PostAsync_200()
         {
             Case sampleCase = this.GenerateBlankCase();
 
@@ -33,7 +33,25 @@ namespace Tests.AsyncTests
         }
 
         [TestMethod]
-        public async Task PaymentTest_UpdateAsync_200()
+        public async Task PaymentTest_NonCard_PostAsync_200()
+        {
+            Case sampleCase = this.GenerateBlankCase();
+
+            Case returnCase = await ApiClient.PostCaseAsync(sampleCase);
+
+            Payment payment = new Payment();
+            payment.PaymentType = Enums.PaymentType.Bitcoin;
+            payment.BINNumber = null;
+
+            Payment returnPayment = await ApiClient.PostPaymentAsync(returnCase.Id, payment);
+
+            Assert.IsTrue(returnPayment.Id != Guid.Empty);
+            Assert.AreEqual(payment.PaymentType, returnPayment.PaymentType);
+            Assert.IsTrue(payment.BINNumber == returnPayment.BINNumber);
+        }
+
+        [TestMethod]
+        public async Task PaymentTest_Card_UpdateAsync_200()
         {
             Case sampleCase = this.GenerateSampleCase();
 
@@ -49,7 +67,25 @@ namespace Tests.AsyncTests
         }
 
         [TestMethod]
-        public async Task PaymentTest_GetAsync_200()
+        public async Task PaymentTest_NonCard_UpdateAsync_200()
+        {
+            Case sampleCase = this.GenerateSampleCase();
+
+            Case returnCase = await ApiClient.PostCaseAsync(sampleCase);
+
+            Payment payment = new Payment();
+            payment.PaymentType = Enums.PaymentType.Bitcoin;
+            payment.BINNumber = null;
+
+            Payment returnPayment = await ApiClient.UpdatePaymentAsync(returnCase.Id, payment, returnCase.Payments[0].Id);
+
+            Assert.IsTrue(returnPayment.Id != Guid.Empty);
+            Assert.AreEqual(payment.PaymentType, returnPayment.PaymentType);
+            Assert.IsTrue(payment.BINNumber == returnPayment.BINNumber);
+        }
+
+        [TestMethod]
+        public async Task PaymentTest_Card_GetAsync_200()
         {
             Case sampleCase = this.GenerateSampleCase();
 
@@ -62,7 +98,23 @@ namespace Tests.AsyncTests
         }
 
         [TestMethod]
-        public async Task PaymentTest_GetAllAsync_200()
+        public async Task PaymentTest_NonCard_GetAsync_200()
+        {
+            Case sampleCase = this.GenerateSampleCase();
+            sampleCase.Payments[0].PaymentType = Enums.PaymentType.Bitcoin;
+            sampleCase.Payments[0].BINNumber = null;
+
+            Case returnCase = await ApiClient.PostCaseAsync(sampleCase);
+
+            Payment returnPayment = await ApiClient.GetPaymentAsync(returnCase.Id, returnCase.Payments[0].Id);
+
+            Assert.IsTrue(returnPayment.Id != Guid.Empty);
+            Assert.AreEqual(returnCase.Payments[0].BINNumber, returnPayment.BINNumber);
+            Assert.AreEqual(returnCase.Payments[0].PaymentType, returnPayment.PaymentType);
+        }
+
+        [TestMethod]
+        public async Task PaymentTest_Card_GetAllAsync_200()
         {
             Case sampleCase = this.GenerateSampleCase();
 
@@ -77,6 +129,29 @@ namespace Tests.AsyncTests
             Assert.IsTrue(returnPayments.Count > 1);
             Assert.AreEqual("123456", returnPayments[0].BINNumber);
             Assert.AreEqual("445566", returnPayments[1].BINNumber);
+        }
+
+        [TestMethod]
+        public async Task PaymentTest_NonCard_GetAllAsync_200()
+        {
+            Case sampleCase = this.GenerateSampleCase();
+            sampleCase.Payments.SingleOrDefault().PaymentType = Enums.PaymentType.Bitcoin;
+            sampleCase.Payments.SingleOrDefault().BINNumber = null;
+
+            Payment payment = new Payment();
+            payment.PaymentType = Enums.PaymentType.PayPal;
+            payment.BINNumber = null;
+            sampleCase.Payments.Add(payment);
+
+            Case returnCase = await ApiClient.PostCaseAsync(sampleCase);
+
+            IList<Payment> returnPayments = await ApiClient.GetPaymentAsync(returnCase.Id);
+
+            Assert.IsTrue(returnPayments.Count > 1);
+            Assert.AreEqual(sampleCase.Payments[0].BINNumber, returnPayments[0].BINNumber);
+            Assert.AreEqual(sampleCase.Payments[0].PaymentType, returnPayments[0].PaymentType);
+            Assert.AreEqual(payment.BINNumber, returnPayments[1].BINNumber);
+            Assert.AreEqual(payment.PaymentType, returnPayments[1].PaymentType);
         }
 
         [TestMethod]
@@ -98,6 +173,25 @@ namespace Tests.AsyncTests
             }
 
             Assert.AreEqual(HttpStatusCode.BadRequest, responseCode);
+        }
+
+        [TestMethod]
+        public async Task PaymentTest_PaymentTypes_Enums_PostAsync_200()
+        {
+            foreach(Enums.PaymentType item in Enum.GetValues(typeof(Enums.PaymentType)))
+            {
+                Case sampleCase = this.GenerateBlankCase();
+
+                Case returnCase = await ApiClient.PostCaseAsync(sampleCase);
+
+                Payment payment = new Payment();
+                payment.PaymentType = item;
+
+                Payment returnPayment = await ApiClient.PostPaymentAsync(returnCase.Id, payment);
+
+                Assert.IsTrue(returnPayment.Id != Guid.Empty);
+                Assert.AreEqual(payment.PaymentType, returnPayment.PaymentType);
+            }
         }
 
         #region SetCaseContents
--- Trustev.Domain/Enums.cs
+++ Trustev.Domain/Enums.cs
@@ -178,7 +178,77 @@ namespace Trustev.Domain
             /// <summary>
             /// BitCoin Payment
             /// </summary>
-            Bitcoin
+            Bitcoin,
+
+            /// <summary>
+            /// Due Payment
+            /// </summary>
+            Due,
+
+            /// <summary>
+            /// Stripe Payment
+            /// </summary>
+            Stripe,
+
+            /// <summary>
+            /// Dwolla Payment
+            /// </summary>
+            Dwolla,
+
+            /// <summary>
+            /// ApplePay Payment
+            /// </summary>
+            ApplePay,
+
+            /// <summary>
+            /// Payoneer Payment
+            /// </summary>
+            Payoneer,
+
+            /// <summary>
+            /// TwoCheckout Payment
+            /// </summary>
+            TwoCheckout,
+
+            /// <summary>
+            /// AmazonPay Payment
+            /// </summary>
+            AmazonPay,
+
+            /// <summary>
+            /// Square Payment
+            /// </summary>
+            Square,
+
+            /// <summary>
+            /// AndroidPay Payment
+            /// </summary>
+            AndroidPay,
+
+            /// <summary>
+            /// LeoPay Payment
+            /// </summary>
+            LeoPay,
+
+            /// <summary>
+            /// GooglePay Payment
+            /// </summary>
+            GooglePay,
+
+            /// <summary>
+            /// SamsungPay Payment
+            /// </summary>
+            SamsungPay,
+
+            /// <summary>
+            /// MasterPass Payment
+            /// </summary>
+            MasterPass,
+
+            /// <summary>
+            /// MicrosoftPay Payment
+            /// </summary>
+            MicrosoftPay
         }
         /// <summary>
         /// This enum represents the ADR Status
